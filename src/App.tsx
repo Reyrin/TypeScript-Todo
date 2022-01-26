bebb7dc72dbfaa78a95a959831a7d778c07bd068
Andrey <raigon28@gmail.com>
2022-01-26 11:48:13 +0300

    adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import "./App.css";
 
 import { ITodo } from "./types/todo";
 
-import TodoList from './components/TodoList'
+import TodoList from "./components/TodoList";
 
 const App: FC = () => {
 	const [value, setValue] = React.useState("");
-	const [todos, setTodos] = React.useState<ITodo[]>([{
-		id: 1643068477497,
-		title: 'Learn React + Redux',
-		completed: true,
-	},
-	{
-		id: 1643068489191,
-		title: 'Learn TypeScript',
-		completed: false,
-	},]);
+	const [todos, setTodos] = React.useState<ITodo[]>([
+		{
+			id: 1643068477497,
+			title: "Learn React + Redux",
+			completed: true,
+		},
+		{
+			id: 1643068489191,
+			title: "Learn TypeScript",
+			completed: false,
+		},
+	]);
 
 	const inputRef = React.useRef<HTMLInputElement>(null);
 
@@ -25,7 +27,7 @@ const App: FC = () => {
 	};
 
 	const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
-		if (e.key === 'Enter') addTodo();
+		if (e.key === "Enter") addTodo();
 	};
 
 	const addTodo = () => {
@@ -39,50 +41,58 @@ const App: FC = () => {
 				},
 			]);
 		} else {
-			alert('Todo не должна быть пустой или слишком большой!')
+			alert("Todo не должна быть пустой или слишком большой!");
 		}
 
-        setValue('');
+		setValue("");
 	};
 
-    const completeTodo = (id: number): void => {
-        setTodos(
-            todos.map(item => {
-                if (item.id === id) {
-                    return {
-                        ...item,
-                        completed: !item.completed
-                    }
-                }
+	const completeTodo = (id: number): void => {
+		setTodos(
+			todos.map((item) => {
+				if (item.id === id) {
+					return {
+						...item,
+						completed: !item.completed,
+					};
+				}
 
-                return item;
-            })
-        );
-    };
+				return item;
+			})
+		);
+	};
 
-    const removeTodo = (id: number): void => {
-        setTodos(todos.filter(todo => todo.id !== id))
-    }
+	const removeTodo = (id: number): void => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
 
 	React.useEffect(() => {
-		if(inputRef.current) inputRef.current.focus();
+		if (inputRef.current) inputRef.current.focus();
 	}, []);
 
 	return (
 		<div className="todo">
 			<h1 className="title">TypeScript Todo List</h1>
-			<input
-				onKeyDown={handleKeyDown}
-				ref={inputRef}
-				type="text"
-				value={value}
-				onChange={handleChange}
-				placeholder="Add a todo..."
-				className="input-todo"
-			/>
-			<button onClick={addTodo} className="add-btn">Add Todo</button>
+			<div className="interface">
+				<input
+					onKeyDown={handleKeyDown}
+					ref={inputRef}
+					type="text"
+					value={value}
+					onChange={handleChange}
+					placeholder="Add a todo..."
+					className="input-todo"
+				/>
+				<button onClick={addTodo} className="add-btn">
+					Add Todo
+				</button>
+			</div>
 
-			<TodoList items={todos} completeTodo={completeTodo} removeTodo={removeTodo}  />
+			<TodoList
+				items={todos}
+				completeTodo={completeTodo}
+				removeTodo={removeTodo}
+			/>
 		</div>
 	);
 };
